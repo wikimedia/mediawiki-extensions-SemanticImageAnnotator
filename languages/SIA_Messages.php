1803dd8bf8155ae916ae3a3b0cd8881821784fe4
feob <f.obenauer@gmail.com>
2012-11-06 00:43:38 +0100

    initial import
    Change-Id: I303bec3231234f20815c819ccdb8242ad6775889

---
+++ languages/SIA_Messages.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Internationalization file for Semantic Image Annotator
+ */
+ $messages = array();
+ /**
+  * English
+  * @author Felix Obenauer
+  */
+ $messages['en'] = array(
+	'sia-description'	=>	'Semantically annotate images and query for annotations.',
+	'sia-comment'		=>	'comment',
+	'sia-buttonannotate'=>  'Annotate this Image',
+	'sia-resize'=>  ' Resize Image '
+ );
+  /**
+  * German
+  * @author Felix Obenauer
+  */
+ $messages['de'] = array(
+	'sia-description'	=>	'Fuege Bildern semantische Annotationen hinzu und frage diese ab.',
+	'sia-comment'		=>	'Kommentar',
+	'sia-buttonannotate'=>  'Dieses Bild annotieren',
+	'sia-resize'=>  ' Bildgröße ändern '
+ );
